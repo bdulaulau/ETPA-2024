00a26e762aeec5c771cdd07424e6ac73acb3cc7b
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2024-10-17 12:19:16 +0200

    Avancé 12h

--- Assets/Scenes/cours_1710.cs
+++ Assets/Scenes/cours_1710.cs
@@ -6,4 +6,37 @@ public class cours_1710 : MonoBehaviour //public = créé un script dispo dans tou
 
     public TMP_Text messageText; //référence à cela pour pouvoir l'utiliser
     public TMP_InputField numberInput; //permet de lire ce que le joueur va écrire dans le chm text
+
+    private int randomNumber; // int= type primitif, dans C# de base 
+
+    private void Start() //void = pas de résultat à nous donner, Start = comeback de lifecycle, à appeler à un moment clé du cycle de notre jeu début/fin
+    {
+        //messageText.text = "Tu as choisi ton destin. Cours.";
+        randomNumber = Random.Range(1, 100 + 1); //Range va prendre le minimum et le maximum
+        Debug.Log("Attention il se passe des trucs chelou, cours mec tu vas mourir. Je déconne c'est : " + randomNumber); //permet d'afficher dans le debug le nombre 
+        messageText.text = "Devine combien de gens sont enterrés dans mon jardin... ?";
+    }
+
+    public void Try()
+    {
+        if (string.IsNullOrWhiteSpace(numberInput.text)) // On teste si il y a que des valeurs vide, retour à la ligne, espace etc etc..
+        {
+            messageText.text = "Tu es sûr.e de ne pas vouloir répondre correctement ? Attention à toi.";
+            numberInput.text = "";
+            return; //pour vider le champs de texte du joueur
+        }
+        if (int.TryParse(numberInput.text, out int playerNumber)) //Lire du texte et va en déduire qqchose, out renvoie une deuxième valeur avec le nombre qu'on a rentré 
+        {
+
+        }
+        else
+        {
+            messageText.text = "Tu es sûr.e de ne pas vouloir répondre coorectement ? Attention à toi.";
+            numberInput.text = "";
+            return; //pour vider le champs de texte du joueur
+
+        }
+    }
+
+
 }
