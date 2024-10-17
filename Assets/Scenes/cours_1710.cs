5711406648e067bfbc012005c6ded28e70f19124
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2024-10-17 12:51:08 +0200

    Fin du cours, programme fonctionnel

--- Assets/Scenes/cours_1710.cs
+++ Assets/Scenes/cours_1710.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine; //Régle d'import, dans ce script on rend ça utilisable 
 public class cours_1710 : MonoBehaviour //public = créé un script dispo dans tout le projet, class = un plan
@@ -11,10 +12,7 @@ public class cours_1710 : MonoBehaviour //public = créé un script dispo dans tou
 
     private void Start() //void = pas de résultat à nous donner, Start = comeback de lifecycle, à appeler à un moment clé du cycle de notre jeu début/fin
     {
-        //messageText.text = "Tu as choisi ton destin. Cours.";
-        randomNumber = Random.Range(1, 100 + 1); //Range va prendre le minimum et le maximum
-        Debug.Log("Attention il se passe des trucs chelou, cours mec tu vas mourir. Je déconne c'est : " + randomNumber); //permet d'afficher dans le debug le nombre 
-        messageText.text = "Devine combien de gens sont enterrés dans mon jardin... ?";
+        Restart();
     }
 
     public void Try()
@@ -27,16 +25,37 @@ public class cours_1710 : MonoBehaviour //public = créé un script dispo dans tou
         }
         if (int.TryParse(numberInput.text, out int playerNumber)) //Lire du texte et va en déduire qqchose, out renvoie une deuxième valeur avec le nombre qu'on a rentré 
         {
+            if (playerNumber == randomNumber)
+            {
+                messageText.text = "Bravo, ah oui toi tu sais trop de chose ..";
+                Restart();
+                return;
+            }
+            else if (playerNumber > randomNumber)
+            {
+                messageText.text = "Un peu moins ..";
+            }
+            else if (playerNumber < randomNumber)
+            {
+                messageText.text = "Un peu plus ..";
+            }
+            else
+            {
+                messageText.text = "Tu es sûr.e de ne pas vouloir répondre coorectement ? Attention à toi.";
+                numberInput.text = "";
+                return; //pour vider le champs de texte du joueur
 
+            }
         }
-        else
-        {
-            messageText.text = "Tu es sûr.e de ne pas vouloir répondre coorectement ? Attention à toi.";
-            numberInput.text = "";
-            return; //pour vider le champs de texte du joueur
 
-        }
     }
-
+    public void Restart()
+    {
+        //messageText.text = "Tu as choisi ton destin. Cours.";
+        randomNumber = Random.Range(1, 100 + 1);//Range va prendre le minimum et le maximum
+        Debug.Log("Nouveau nombre : " + randomNumber);//permet d'afficher dans le debug le nombre 
+        messageText.text = "C'est reparti !";
+    }
+    
 
 }
