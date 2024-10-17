0361be274325147ddc19e0714c808a1ac626200c
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2024-10-17 10:43:53 +0200

    On sait jamais

---
+++ Assets/Scenes/cours_1710.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine; //Régle d'import, dans ce script on rend ça utilisable 
+public class cours_1710 : MonoBehaviour //public = créé un script dispo dans tout le projet, class = un plan
+//MonoBehaviour = class de base pour créer des composants custom
+{
+
+    public TMP_Text messageText; //référence à cela pour pouvoir l'utiliser
+    public TMP_InputField numberInput; //permet de lire ce que le joueur va écrire dans le chm text
+}
